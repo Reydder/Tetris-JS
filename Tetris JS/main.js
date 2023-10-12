0c291088c48b5866f89cc89f70aeee2d26d228ca
Javier <javiermolinalorente@gmail.com>
2023-10-12 17:34:08 +0200

    Paint board with matrix

--- Tetris JS/main.js
+++ Tetris JS/main.js
@@ -1,16 +1,51 @@
 import './style.css'
 
+const BLOCK_SIZE = 20
+const BOARD_WIDTH = BLOCK_SIZE * 10
+const BOARD_HEIGHT = BLOCK_SIZE * 20
+
 const canvas = document.getElementsByTagName("canvas").item(0)
+const ctx = canvas.getContext("2d")
+
+ctx.canvas.width = BOARD_WIDTH
+ctx.canvas.height = BOARD_HEIGHT
+
+const BOARD = [
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+]
 
 function draw() {
-  const ctx = canvas.getContext("2d")
+  ctx.fillStyle = '#FFF'
+  ctx.fillRect(0, 0, BOARD_WIDTH, BOARD_HEIGHT)
 
-  ctx.canvas.width = 300
-  ctx.canvas.height = 600
+  ctx.fillStyle = '#000'
 
-  ctx.fillStyle = '#FAF'
-
-  ctx.fillRect(0, 0, 300, 600)
+  BOARD.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value == 1) {
+        ctx.fillRect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
+      }
+    })
+  })
 }
 
 draw()
