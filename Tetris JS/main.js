5e9a72845844e6095aae0fa4594f890cbc9bf6bc
Javier <javiermolinalorente@gmail.com>
2023-10-12 15:02:45 +0200

    Paint board

--- Tetris JS/main.js
+++ Tetris JS/main.js
@@ -1,2 +1,16 @@
 import './style.css'
 
+const canvas = document.getElementsByTagName("canvas").item(0)
+
+function draw() {
+  const ctx = canvas.getContext("2d")
+
+  ctx.canvas.width = 300
+  ctx.canvas.height = 600
+
+  ctx.fillStyle = '#FAF'
+
+  ctx.fillRect(0, 0, 300, 600)
+}
+
+draw()
