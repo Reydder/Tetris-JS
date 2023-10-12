d412cb945e048965e51364a9179d2f53243e5d07
Javier <javiermolinalorente@gmail.com>
2023-10-12 19:24:29 +0200

    Collisions

--- Tetris JS/main.js
+++ Tetris JS/main.js
@@ -11,8 +11,8 @@ ctx.canvas.width = BOARD_WIDTH
 ctx.canvas.height = BOARD_HEIGHT
 
 const piece = {
-  x: 5,
-  y: 5,
+  x: 4,
+  y: 0,
   shape: [
     [1, 1],
     [1, 1]
@@ -46,19 +46,26 @@ function loop() {
   window.requestAnimationFrame(draw)
 
   document.addEventListener('keydown', (event) => {
-
     switch (event.key) {
       case 'ArrowLeft':
         piece.x--
+        if (isGoingToCollide()) {
+          piece.x++
+        }
         break
       case 'ArrowRight':
         piece.x++
+        if (isGoingToCollide()) {
+          piece.x--
+        }
         break
       case 'ArrowDown':
         piece.y++
+        if (isGoingToCollide()) {
+          piece.y--
+        }
         break
     }
-
   })
 }
 
@@ -90,6 +97,27 @@ function paintCurrentPiece() {
 function paintBlock(x, y, color) {
   ctx.fillStyle = color
   ctx.fillRect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
+}
+
+function isGoingToCollide() {
+  var goingToCollide = false
+
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      const xBoardCoordinate = x + piece.x
+      const yBoardCoordinate = y + piece.y
+      
+      if (value = 1) {
+        if (BOARD[yBoardCoordinate] == undefined ||
+            BOARD[yBoardCoordinate][xBoardCoordinate] == undefined ||
+            BOARD[yBoardCoordinate][xBoardCoordinate] == 1) {
+          goingToCollide = true
+        }
+      }
+    })
+  })
+
+  return goingToCollide
 }
 
 loop()
